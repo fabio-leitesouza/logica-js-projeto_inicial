a5098719eaab9588a7004f2ebf28dd6526fcf2ec
fabio-souza <101989744+fabio-leitesouza@users.noreply.github.com>
2024-03-27 11:12:52 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-alert('Bem vindo ao jogo do número secreto');
-
 let numeroSecreto = Math.floor(Math.random() * 5);
-let numeroDigitado = parseInt(prompt('Digite um número de 1 a 5'));
+let tentativas = 3;
 
-if(numeroDigitado === numeroSecreto){
-    alert('Parabéns, você acertou!');
-} else if(numeroDigitado > numeroSecreto){
-    alert('Você digitou um número maior que o número secreto');
-} else {
-    alert('Você digitou um número menor que o número secreto');
+while (tentativas > 0) {
+    let numeroDigitado = parseInt(prompt('Digite um número de 1 a 5'));
+    if(numeroDigitado === numeroSecreto){
+        alert('Parabéns, você acertou!');
+        break;
+    } else if(numeroDigitado > numeroSecreto){
+        alert('Você digitou um número maior que o número secreto');
+    } else {
+        alert('Você digitou um número menor que o número secreto');
+    }
+    alert(`você ainda tem  ${tentativas} tentativas`); 
+    tentativas = tentativas - 1;
 }
