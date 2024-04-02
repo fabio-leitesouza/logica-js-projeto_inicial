29194d81a4592532afbba425c0cbd02a5a65c20e
fabio-souza <101989744+fabio-leitesouza@users.noreply.github.com>
2024-04-02 09:16:51 -0300

    updategame

--- app.js
+++ app.js
@@ -1,31 +1,40 @@
-somaNumeros(1, 2);
-let numeroSecreto = Math.floor(Math.random() * 5 + 1);
-// let tentativas = 1;
 
-//while é para repetir o código enquanto o numero de tentativas for menor que 4
+function gerarNumeroAleatorio(index){
+    return Math.floor(Math.random() * index + 1);
+}
 
-for(let tentativas = 0; tentativas < 3; tentativas++){
-    let numeroDigitado = parseInt(prompt('Digite um número de 1 a 5'));
-    if(numeroDigitado === numeroSecreto){
-        alert('Parabéns, você acertou!');
-        break;
-    } else if(numeroDigitado > numeroSecreto){
-        alert('Você digitou um número maior que o número secreto');
-    } else {
-        alert('Você digitou um número menor que o número secreto');
+function jogo () {
+    let numeroDePosicoesAleatorias = parseInt(prompt('Quantos númeos você quer aleatorizar'))
+    let numeroDeTentativas = parseInt(prompt('Em quantas tentativas você quer tentar adivinhas?'))
+    let numeroSecreto = gerarNumeroAleatorio(numeroDePosicoesAleatorias);
+
+    for(let tentativas = 0; tentativas < numeroDeTentativas; tentativas++){
+        let numeroDigitado = parseInt(prompt(`Digite um número de 1 a ${numeroDePosicoesAleatorias}`));
+        if(numeroDigitado === numeroSecreto){
+            alert('Parabéns, você acertou!');
+            break;
+        } else if(numeroDigitado > numeroSecreto){
+            alert('Você digitou um número maior que o número secreto');
+        } else {
+            alert('Você digitou um número menor que o número secreto');
+        }
+        if(tentativas === numeroDeTentativas){
+            alert(`Suas tentativas acabaram`);
+        } else if (tentativas <= numeroDeTentativas){
+            alert(`você ainda tem  ${numeroDeTentativas - tentativas - 1} tentativas`);   
+        }else{
+            alert(`você ainda tem  ${numeroDePosicoesAleatorias - tentativas - 1} tentativa`);
+        }    
     }
-    if(tentativas === 3){
-        alert(`Suas tentativas acabaram`);
-    } else if (tentativas <2){
-        alert(`você ainda tem  ${2 - tentativas} tentativas`);   
-    }else{
-        alert(`você ainda tem  ${2 - tentativas} tentativa`);
-    }    
+    let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+    if(tentativas < 4){
+        alert(`Você acertou com ${tentativas} ${palavraTentativa}`)
+    }
+} 
+
+function clicou() {
+    jogo();
 }
 
-//palavraTentativa recebendo o resultado de um tenário (ou 'tentativas' ou 'tentativa')
-let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
-if(tentativas < 4){
-    alert(`Você acertou com ${tentativas} ${palavraTentativa}`)
-}
+
 
