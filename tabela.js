35a9aa2e3de32b2d699b290f9412b381a68e6c6c
fabio-souza <101989744+fabio-leitesouza@users.noreply.github.com>
2024-04-08 14:00:03 -0300

    Update tabela.js

--- tabela.js
+++ tabela.js
@@ -1,3 +1,22 @@
+function gerarTabela(){
+    let linhas = parseInt(prompt('Digite o número de linhas da sua tabela'));
+    let colunas = parseInt(prompt('Digite o número de colunas da sua tabela'));
+
+    let tabela = [];
+    for(let i = 0; i < linhas; i++){
+        tabela[i] = [];
+        for(let j = 0; j < colunas; j++){
+            tabela[i][j] = gerarNumeroAleatorio(100);
+        }
+    }
+    return tabela;
+}
+
+console.table(gerarTabela());
+
+
+
+
 let tabelaEntrada = [
     [0, 0, 0],
     [0, 0, 0, 0],
