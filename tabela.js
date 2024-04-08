cab8062a7b0695f1e7f34aaf0fcee96c7c42ec52
fabio-souza <101989744+fabio-leitesouza@users.noreply.github.com>
2024-04-08 09:07:31 -0300

    Update tabela.js

--- tabela.js
+++ tabela.js
@@ -2,12 +2,14 @@ let atividade = [
     ['acorda', 5],
     ['aula', 7]
 ]
+let tabelaTexto;
 
 for(let i = 0; i < 2; i++){
     for(let j = 0; j < 2; j++){
-        alert(atividade[i][j])
+        
     }
 }
 
 let matrizString = JSON.stringify(atividade);
 alert(matrizString);
+console.table(atividade);
