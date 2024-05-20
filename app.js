f14e041bc3b2ccfd3ac01ad392295c60dd8425ef
fabio-souza <101989744+fabio-leitesouza@users.noreply.github.com>
2024-05-20 08:30:46 -0300

    update

--- app.js
+++ app.js
@@ -1,30 +1,33 @@
-alert("Bem vindo ao jogo do número secreto");
+// alert("Bem vindo ao jogo do número secreto");
 
-let numeroSecreto = parseInt(Math.random() * 10 + 1);
-console.log(numeroSecreto);
+
 let chute;
 let tentativas = 3;
 
-while (tentativas > 0) {
-    chute = parseInt(prompt("Digite um número de 1 a 10: "));
-    if(chute === numeroSecreto){
-        console.log("Você acertou");
-        alert(`Parabéns vc acertou! O número secretop é: ${chute}`);
-        break;
-    } else {
-        if(chute > numeroSecreto){
-            alert(`O número secreto é menor que ${chute}` );
+function verificaChute() {  
+    let numeroSecreto = parseInt(Math.random() * 10 + 1);
+    console.log(numeroSecreto);
+    while (tentativas > 0) {
+        chute = parseInt(prompt("Digite um número de 1 a 10: "));
+        if(chute === numeroSecreto){
+            console.log("Você acertou");
+            alert(`Parabéns vc acertou! O número secretop é: ${chute}`);
+            break;
         } else {
-            alert(`O número secreto é maior que ${chute}`);
-        }
-        tentativas--;
-        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
-        
-        alert(`Você ainda tem ${tentativas} ${palavraTentativa}`)
-        if(tentativas === 0){
-            alert("Suas tentativas acabaram");
-        }
+            if(chute > numeroSecreto){
+                alert(`O número secreto é menor que ${chute}` );
+            } else {
+                alert(`O número secreto é maior que ${chute}`);
+            }
+            tentativas--;
+            let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+            
+            alert(`Você ainda tem ${tentativas} ${palavraTentativa}`)
+            if(tentativas === 0){
+                alert("Suas tentativas acabaram");
+            }
 
+        }
     }
 }
 
